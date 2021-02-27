bd97e143ff87bd28a1679190d5a4586774f13f04
SansoNoia <devsanso@outlook.com>
2021-02-27 22:58:03 +0900

    main function

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"net"
+	"log"
+	"strconv"
+	"google.golang.org/grpc"
+)
+
 func main() {
+	cfg,cfgErr := newServerArgs()
+	if cfgErr != nil {log.Panicln(cfgErr)}
+
+	l,lErr := net.Listen("tcp",cfg.Host + ":" + strconv.Itoa(cfg.Port))
+	if lErr != nil {log.Panicln(lErr)}
+
+	grpcServer := grpc.NewServer()
+	log.Println("start cloudAuthServer -> ",cfg.Host+":",cfg.Port)
 	
+	if grpcErr := grpcServer.Serve(l);grpcErr != nil {
+		log.Panicln(grpcErr)
+	}
 }

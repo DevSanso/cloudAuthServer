a044fb4848f3404a0e06f65a34bdca327d83d25f
SansoNoia <devsanso@outlook.com>
2021-03-11 19:08:52 +0900

    yep done

--- db/init/init_db.go
+++ db/init/init_db.go
@@ -1,13 +1,14 @@
 package init
+
 import (
+	"cloudAuthServer/db/document"
 	"context"
-	"errors"
+	"log"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"cloudAuthServer/db/document"
-	
-)
+	"go.mongodb.org/mongo-driver/x/bsonx"
+)	
 
 func InitDb(client *mongo.Client,dbName string) {
 	var db = client.Database(dbName)
@@ -19,6 +20,14 @@ func InitDb(client *mongo.Client,dbName string) {
 
 func createUser(db *mongo.Database) {
 	db.CreateCollection(context.Background(),document.UserName)
+	_,err := db.Collection(document.UserName).Indexes().
+		CreateOne(context.Background(),mongo.IndexModel{
+			Keys: bsonx.Doc {{document.UserCollectionEmailFieldName,bsonx.String("")}},
+			Options : options.Index().SetUnique(true),
+		});
+
+	if err != nil {log.Panicln(err)}
+	
 }
 
 func createMapping(db *mongo.Database) {

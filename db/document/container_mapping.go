ffe8e95f26bad2d39d0c6d4e9bc784b579953bb7
SansoNoia <devsanso@outlook.com>
2021-03-15 18:25:20 +0900

    add tobson

--- db/document/container_mapping.go
+++ db/document/container_mapping.go
@@ -1,8 +1,23 @@
 package document
 
+import (
+	"go.mongodb.org/mongo-driver/bson"
+)
+
 type ContainerMapping struct {
-	userId int `bson:userId`
+	UserId int `bson:userId`
 	ContainerId int `bson:cotainerId`
+}
+const (
+	CMCollectionUserIdFieldName = "userId"
+	CmCollectionContainerIdFieldName = "cotainerId"
+)
+func(cm *ContainerMapping)ToBson() bson.D {
+	return bson.D {
+		{CMCollectionUserIdFieldName,cm.UserId},
+		{CmCollectionContainerIdFieldName,cm.ContainerId},
+	}
+	
 }
 
 const MappingName = "container_mapping"
